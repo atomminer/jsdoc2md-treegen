981651c26ee013243371147715836d8df5ad6bcc
Unknown <atom@atomminer.com>
2020-12-31 03:19:32 +0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,100 @@
-# jsdoc2md-treegen
-jsdoc2md-treegen is a simple jsdoc2md cli wrapper to generate markdown module tree with index
+[![node version](https://img.shields.io/badge/node-%3E%3D%2010.0.0-brightgreen?style=plastic)](https://img.shields.io/badge/node-%3E%3D%2010.0.0-brightgreen?style=plastic) [![GitHub code size in bytes](https://img.shields.io/github/languages/code-size/atomminer/jsdoc2md-treegen?style=plastic)](https://img.shields.io/github/languages/code-size/atomminer/jsdoc2md-treegen?style=plastic) [![DeepScan grade](https://deepscan.io/api/teams/12301/projects/15297/branches/303459/badge/grade.svg)](https://deepscan.io/dashboard#view=project&tid=12301&pid=15297&bid=303459)
+
+## jsdoc2md-treegen
+
+`jsdoc2md-treegen` is a simple `jsdoc2md` cli wrapper to generate markdown module tree with index.  While `jsdoc2md` is really nice and useful tool, it doesn't feel like a good idea to dump literally everything into a single .md file. markdown tree seems to be more intuitive and convenient to navigate through.
+
+#### Installtion
+```bash
+npm install -g atomminer/jsdoc2md-treegen
+```
+### How it works
+Add `.jsdoc2md-treegen.json` in your project root and run `jsdoc2md-treegen` to generate markdown tree. No command line arguments required nor supported. Everything is taken from `.jsdoc2md-treegen.json` and `package.json` files to generate API documentation for your project.
+
+Generated markdown tree structure follows `@module` definitions, not the source file location, which provides enough flexibility to create logical and readable API docs.  For example,
+source file located at `src/helpers/my_view.js` with following module definition:
+```js
+/**
+* My super cool view controller
+* @module  myproject/controllers/myview
+* @typicalname controller
+*/
+```
+Will be translated to `docs/myproject/controllers/myview.md` in the documentation tree.
+
+#### .jsdoc2md-treegen.json configuration file
+| Name | Type | Default | Description |
+| --- | --- | --- | --- |
+|output|`string`|`'docs'`| Output documentation folder. docs will be used if missing|
+|cleanOutput|`boolean`|`false`| Empty output folder on start. :warning::warning::warning: will remove _**all**_ files and folders in the output folder if `true` :warning::warning::warning: |
+|input|`string|Array`|`'src/**/*.js'`| Filepaths (`**` glob matching supported) of javascript source files to parse. Passed to `jsdoc2md.files`. |
+|index|`boolean|string`|`false`| Documentation index filename. If `true` `index.md` will be created. Ex: `'README.md'`|
+|indexTemplate|`boolean|string`|`false`| Optional documentation index template filename. |
+|moduleTemplate|`boolean|string`|`false`| Optional single module template filename. |
+|namepaths|`boolean|string`|`false`| JSON namepaths output filename. `namepaths.json` will be used if `true`. |
+|jsdoc2md|`Object`|`{}`| `jsdoc2md` [options object](https://github.com/jsdoc2md/jsdoc-to-markdown/blob/master/docs/API.md#jsdoctomarkdown-) Passed directly to `jsdoc2md` |
+
+#### Default templates
+#####  Single module default template
+```
+{{#module name="${moduleName}"}}
+{{>docs}}
+{{/module}}
+```
+where `${moduleName}` is replaced with the current module name. 
+
+To add header and footer:
+```
+![node version](https://img.shields.io/badge/node-%3E%3D%2010.0.0-brightgreen?style=plastic)
+---
+
+{{#module name="${moduleName}"}}
+{{>docs}}
+{{/module}}
+
+---
+Copyright (c) 2020 AtomMiner
+```
+#####  Index default template
+```
+## {{name}} Reference API
+
+{{description}}
+
+#### Modules
+
+{{index}}
+```
+`{{name}}` and `{{description}}` will be taken from `package.json` and `{{index}}` will be replaced with the actual index.
+
+#### Examples
+`jsdoc2md-treegen` follows `@module` names to build project tree structure. For example, following set of source files:
+```
+📂 src
+┣ 📜transport.js   		// * @module  transport
+┣ 📜tcp.js				// * @module  tcp/tcp
+```
+will generate following markdown tree because `tcp.js` module definition `@module  tcp/tcp`.
+```
+📂 docs
+┣ 📂 tcp
+┣ ┗ 📜tcp.md
+┣ 📜transport.md
+┣ 📜index.md
+┣ 📜namepaths.json
+```
+---
+To setup npm scripts, simly add `.jsdoc2md-treegen.json`(can be empty JSON object `{}` for all default settings) to your project and add script to `package.json`:
+```json
+"scripts": {
+  ...
+  "gen-docs": "jsdoc2md-treegen"
+},
+```
+then, to (re)generate your project documentation run:
+```bash
+npm run gen-docs
+```
+
+---
+Copyright (c) 2020 AtomMiner. This project is released under MIT license.
---
+++ index.js
@@ -0,0 +1,166 @@
+#!/usr/bin/env node
+
+const jsdoc2md = require('jsdoc-to-markdown');
+const fs = require('fs-extra')
+const path = require('path');
+const { Console } = require('console');
+
+console.log(__dirname);
+console.log(process.cwd());
+const errorAndExit = (e) => {
+	console.error('\x1b[31m', e, '\x1b[0m');
+	process.exit(255);
+}
+
+// Banner
+console.log('This is extension cli tool for jsdoc2md to generate project .md tree instead of dumping everything into a single file.');
+console.log('All settings are picked up from .jsdoc2md-treegen.json configuration file.');
+console.log('');
+
+var conf = null;
+try {
+	conf = JSON.parse(fs.readFileSync(path.resolve(process.cwd(), '.jsdoc2md-treegen.json')));
+}
+catch(e) {
+	errorAndExit(e);
+}
+
+if(!conf) errorAndExit('Non-empty .jsdoc2md-treegen.json config file required. Cancelled.');
+if(!conf.input) {
+	conf.input = '**/*.js';
+	console.warn('No input specified. Using **/*.js');
+}
+if(!Array.isArray(conf.input) && typeof conf.input !== 'string') errorAndExit('Input must be either filename, glob or array');
+if(!conf.output) {
+	conf.output = 'docs';
+	console.log('No output folder specified. Using ./docs');
+}
+
+const outdir = path.resolve(process.cwd(), conf.output);
+// delete all files and folders in output folder
+if(conf.cleanOutput) {
+	console.log(`Cleaning output folder "${conf.output}"...`);
+	try {
+		fs.emptyDirSync(outdir)
+	}
+	catch(e) {
+		console.error(e);
+	}
+}
+
+// jsdoc2md options
+var options = conf.jsdoc2md || {};
+if(!options.files) {
+	if(Array.isArray(conf.input)) options.files = conf.input.join(' ');
+	else options.files = conf.input;
+}
+
+const templateData = jsdoc2md.getTemplateDataSync(options);
+const moduleNames = templateData.filter(({kind}) => kind === 'module').map(({name}) => name);
+
+
+var indexTemplate = null;
+var moduleTemplate = null;
+//////////////////////////////////////////////////////////////////////////////////////////////
+// Try loading single module template and index template
+if(conf.moduleTemplate) {
+	try {
+		moduleTemplate = fs.readFileSync(path.resolve(process.cwd(), conf.moduleTemplate), 'utf8');
+	}
+	catch(e) {
+		console.warn('Failed to load module template. Using deafult. Err: ', e);
+	}
+}
+
+if(conf.indexTemplate) {
+	try {
+		indexTemplate = fs.readFileSync(path.resolve(process.cwd(), conf.indexTemplate), 'utf8');
+	}
+	catch(e) {
+		console.warn('Failed to load index template. Using deafult. Err: ', e);
+	}
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+// Write modules
+const renderModule = (moduleName, data) => {
+	const template = moduleTemplate ? moduleTemplate.replace(/\$\{moduleName\}/g, moduleName) : 
+		`{{#module name="${moduleName}"}}{{>docs}}{{/module}}`;
+	const moduleDocumentation = jsdoc2md.renderSync({data, template});
+
+	try {
+		const location = path.resolve(outdir, `${moduleName}.md`);
+		fs.ensureFileSync(location);
+		fs.writeFileSync(location, moduleDocumentation);
+	}
+	catch(e) {
+		console.error(`Error processing ${moduleName}: `, e);
+	}
+}
+
+for(var m of moduleNames) {
+	const modulePath = path.join(conf.output, `${m}.md`);
+	console.log(`Processing module ${m} --> ${modulePath}`);
+	renderModule(m, templateData);
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+// Write index
+if(conf.index) {
+	var indexname = typeof conf.index === 'string' ? conf.index : 'index.md';
+	if(!indexname.toLowerCase().endsWith('.md')) indexname += '.md'
+	const location = path.resolve(outdir, `${indexname}`);
+	console.log(`Processing index --> ${indexname}`);
+	
+	var index = moduleNames.sort().map(moduleName => `* [${moduleName}](${moduleName}.md)`).join('\r\n');
+	var template = indexTemplate;
+	var pkg = {};
+	try {
+		const location = path.resolve(process.cwd(), 'package.json');
+		pkg = JSON.parse(fs.readFileSync(location, 'utf8'));
+	}
+	catch(e) {
+		console.warn('Error reading package.json');
+	}
+
+	if(!template) {
+		template =  
+`## {{name}} Reference API
+
+{{description}}
+
+#### Modules
+
+{{index}}
+`;
+	}
+
+	try {
+		fs.ensureFileSync(location);
+		var data = template.replace(/\{\{index\}\}/g, index)
+				.replace(/\{\{name\}\}/g, pkg.name || '')
+				.replace(/\{\{description\}\}/g, pkg.description || '')
+		fs.writeFileSync(location, data);
+	}
+	catch(e) {
+		console.error(`Error processing ${indexname}: `, e);
+	}
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+// Write namepaths
+if(conf.namepaths) {
+	var filename = typeof conf.namepaths === 'string' ? conf.namepaths : 'namepaths.json';
+	if(!filename.toLowerCase().endsWith('.json')) filename += '.json'
+	const location = path.resolve(outdir, filename);
+	console.log(`Generating namepaths --> ${filename}`);
+	try {
+		fs.ensureFileSync(location);
+		fs.writeFileSync(location, JSON.stringify(templateData, null, 2));
+	}
+	catch(e) {
+		console.error(`Error writing namepaths: `, e);
+	}
+}
+
+
